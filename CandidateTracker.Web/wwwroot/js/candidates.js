b4b2162a6e5acb4f612ddf5e96dab7f3506f4cc1
Ahuva Klein <ahuvalklein@gmail.com>
2019-05-30 22:58:40 -0400

    Commit 1

--- CandidateTracker.Web/wwwroot/js/candidates.js
+++ CandidateTracker.Web/wwwroot/js/candidates.js
@@ -31,10 +31,4 @@
 
     })
 
-    //$('.navbar-nav .nav-link').click(function () {
-    //    $('.navbar-nav .nav-link').removeClass('active');
-    //    $(this).addClass('active');
-    //    console.log('hello');
-    //})
-
 })
